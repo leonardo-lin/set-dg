42ec5b7194ab1978797a355c05a82823c379e74c
leonardo-lin <st950065@yahoo.com.tw>
2023-05-24 20:55:08 +0800

    add bart and bleu score
    add bart score
    add idf_bleu score
    add bleu score

--- indicator.py
+++ indicator.py
@@ -1,10 +1,11 @@
 def bleu_score(candidate, references):
-    """
-    Compute the BLEU score given a candidate and references
-    :param candidate: list of tokens
-    :param references: list of list of tokens
-    :return: BLEU score dict
-    """
+    
+    from nltk.translate.bleu_score import sentence_bleu
+    bleu1_score=sentence_bleu(references,candidate,weights=(1,0,0,0))
+    bleu2_score=sentence_bleu(references,candidate,weights=(0,1,0,0))
+    bleu3_score=sentence_bleu(references,candidate,weights=(0,0,1,0))
+    bleu4_score=sentence_bleu(references,candidate,weights=(0,0,0,1))
+    return [bleu1_score,bleu2_score,bleu3_score,bleu4_score]
 
 
 def rouge_score(candidate, references):
@@ -23,6 +24,47 @@ def bert_score(candidate, references):
     :param references: list of list of tokens
     :return: BERT score dict
     """
+def bart_score(candidate):
+    from bart_score import BARTScorer
+    bart_scorer = BARTScorer(device='cuda', checkpoint='facebook/bart-large-cnn')
+    tokens = ' '.join(map(str, candidate)) #make all the word into a sentence
+    tokens=tokens+'.' #a sentence need a period
+    return bart_scorer.score(['Such as'], [tokens],batch_size=4)
+
+
+def idf_bleu_score(candidate,reference,token):
+    #token = how many grams for bleu
+    import re
+    import json
+    score=[]
+    with open("./raceidf.json",'r',encoding='utf-8')as f:
+        idf_lib=json.load(f)
+    for one_reference in reference:
+        print(reference)
+        reference_ngrams = [one_reference[i:i+token] for i in range(len(one_reference)-(token-1))]
+        candidate_ngrams = [candidate[i:i+token] for i in range(len(candidate)-(token-1))]
+        print(reference_ngrams)
+        print(candidate_ngrams)
+        candidate_totscore=[]
+        for i in candidate_ngrams:
+            tmp_score=0
+            for j in i:
+                if j not in idf_lib.keys():
+                    idf_lib[j]=1
+                tmp_score+=idf_lib[j]
+            candidate_totscore.append(tmp_score)
+        count = 0
+        for ngram in range(len(candidate_ngrams)):
+            count += reference_ngrams.count(candidate_ngrams[ngram])*candidate_totscore[ngram]
+            
+        try:
+            bleu_score = count / sum(candidate_totscore)
+        except:
+            bleu_score=0
+        score.append(bleu_score)
+    if len(score)==0:
+        return 0
+    return max(score)
 
 
 class UnifiedQA_Scorer:
